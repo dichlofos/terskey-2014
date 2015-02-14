6d6dae6905e079277a9735ed9ce5147bb55ea82e
Mikhail Veltishchev <dichlofos-mv@yandex.ru>
2015-02-14 13:25:02 +0300

    Support time offset and other command line args

--- track/split.py
+++ track/split.py
@@ -7,8 +7,11 @@ Written by Mikhail Veltishchev <dichlofos-mv@yandex.ru>
 """
 
 import os
+import sys
 import dateutil.parser
 import datetime
+import argparse
+
 
 def _read_lines(file_name):
     """ Read file by lines. """
@@ -19,17 +22,19 @@ def _read_lines(file_name):
     return lines
 
 
-def _fix_time(point_line):
+def _fix_time(point_line, time_offset=6):
     """ Fix time offset. """
     if not '<time' in point_line:
         return point_line
 
+    # kyrgyzstan time offset GMT+6
     time = point_line.replace('<time>', '').replace('</time>', '')
-    line_datetime = dateutil.parser.parse(time) + datetime.timedelta(hours=6)
+
+    line_datetime = dateutil.parser.parse(time) + datetime.timedelta(hours=time_offset)
     return '<time>' + line_datetime.isoformat() + '</time>\n'
 
 
-def filter_by_day(lines, day, output_file_name):
+def filter_by_day(lines, day, output_file_name, time_offset):
     """ Filter track items by day and fix dates. """
     started = False
     cur_point = None
@@ -49,13 +54,15 @@ def filter_by_day(lines, day, output_file_name):
             cur_point.append(line)
             started = False
             # process point
-            good = False
             good = any(mask in l for l in cur_point)
 
             if good:
                 some_points = True
                 for point_line in cur_point:
-                    point_line = _fix_time(point_line)
+                    point_line = _fix_time(
+                        point_line=point_line,
+                        time_offset=time_offset,
+                    )
                     output_file.write(point_line)
             continue
 
@@ -75,7 +82,37 @@ def filter_by_day(lines, day, output_file_name):
 
 def main():
     """ Gravicappa. """
-    file_name = 'track.gpx'
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-t", "--time-offset",
+        type=int,
+        help="Time offset")
+
+    parser.add_argument(
+        "-i", "--track",
+        type=str,
+        help="Input track name"
+    )
+
+    parser.add_argument(
+        "-w", "--waypoints",
+        type=str,
+        help="Input waypoints name"
+    )
+
+    args = parser.parse_args()
+
+    time_offset = 6
+    if args.time_offset is not None:
+        time_offset = args.time_offset
+    print("Using time offset", time_offset)
+
+    file_name = args.track
+    if not file_name.endswith('.gpx'):
+        print "I cannot handle non-GPX-tracks"
+        sys.exit(1)
+
     lines = _read_lines(file_name)
 
     for day in xrange(1, 30):
@@ -85,14 +122,21 @@ def main():
 
         output_file_name = file_name.replace('.gpx', '-{}.gpx'.format(sday))
         print "Output", sday, "to", output_file_name
-        filter_by_day(lines, sday, output_file_name)
-
+        filter_by_day(
+            lines=lines,
+            day=sday,
+            output_file_name=output_file_name,
+            time_offset=time_offset,
+        )
 
     output_wpt_file = open('Waypoints-composite-local.gpx', 'w')
 
-    wpt_lines = _read_lines('Waypoints-composite.gpx')
+    wpt_lines = _read_lines(args.waypoints)
     for line in wpt_lines:
-        line = _fix_time(line)
+        line = _fix_time(
+            point_line=line,
+            time_offset=time_offset,
+        )
         output_wpt_file.write(line)
 
 
